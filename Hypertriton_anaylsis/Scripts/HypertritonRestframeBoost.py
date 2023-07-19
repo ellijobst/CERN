c7304116c7adc3740bdb43fec98dd1e59c79c771
ellijobst <elli.jobst@gmx.de>
2023-07-19 16:37:03 +0200

    Completed Hypertriton Rest Frame Boost Script

--- Hypertriton_anaylsis/Scripts/HypertritonRestframeBoost.py
+++ Hypertriton_anaylsis/Scripts/HypertritonRestframeBoost.py
@@ -2,11 +2,13 @@
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+# import ROOT
 import matplotlib.pyplot as plt
 from hipe4ml.model_handler import ModelHandler
 from hipe4ml.tree_handler import TreeHandler
 from hipe4ml import plot_utils
 from matplotlib.backends.backend_pdf import PdfPages
+
 
 
 
@@ -26,7 +28,7 @@ def get_pT(px,py):
 
 #cuts 
 #pseudorapidity < 0.8
-def get_peudorapidity(px,py,pz):
+def get_pseudorapidity(px,py,pz):
     p = np.sqrt(px**2+py**2+pz**2)
     eta = 1/2* np.log((p+pz)/(p-pz))
     return eta
@@ -43,7 +45,11 @@ def get_m_Hyp(px_He,py_He,pz_He, px_Pi,py_Pi,pz_Pi, px_Hyp, py_Hyp,pz_Hyp):
 
     return m_Hyp
 
+def get_cos_theta_beam(qx_He, qy_He, qz_He):
+    return qz_He/np.sqrt(qx_He**2+qy_He**2+qz_He**2)
 
+def get_cos_theta_pHyp(qx_He, qy_He, qz_He, px_Hyp, py_Hyp, pz_Hyp):
+    return (qx_He*px_Hyp+qy_He*py_Hyp+qz_He*pz_Hyp)/np.sqrt(qx_He**2+qy_He**2+qz_He**2)*np.sqrt(px_Hyp**2+py_Hyp**2+pz_Hyp**2)
 
 def get_CMSFourVector(m,px,py,pz,m_Hyp, px_Hyp, py_Hyp, pz_Hyp):
     
@@ -67,6 +73,8 @@ def get_CMSFourVector(m,px,py,pz,m_Hyp, px_Hyp, py_Hyp, pz_Hyp):
     return [E_CMS, px_CMS, py_CMS, pz_CMS]
 
 def boost(df, i):
+    if i%100==0:
+        print(i)
     px_Pi = df['pxPi'][i]
     py_Pi = df['pyPi'][i]
     pz_Pi = df['pzPi'][i]
@@ -83,24 +91,56 @@ def boost(df, i):
 
     FourVector_Hyp = get_CMSFourVector(m_Hyp,px_Hyp, py_Hyp, pz_Hyp, m_Hyp, px_Hyp, py_Hyp, pz_Hyp )
     FourVector_He3 = get_CMSFourVector(m_He,px_He, py_He, pz_He, m_Hyp, px_Hyp, py_Hyp, pz_Hyp )
-    FourVector_Pi = get_CMSFourVector(m_Pi,px_Pi, py_Pi, pz_Pi, m_Hyp, px_Hyp, py_Hyp, pz_Hyp )
+    FourVector_Pi = get_CMSFourVector(m_Pi,px_Pi, py_Pi, pz_Pi, m_Hyp, px_Hyp, py_Hyp, pz_Hyp )#
 
+    FourVectors = FourVector_Hyp+FourVector_He3+FourVector_Pi
+    # return (FourVector_Hyp, FourVector_He3, FourVector_Pi)
+    return FourVectors
+#['pt', 'rapidity', 'phi', 'ct', 'pxHe3', 'pyHe3', 'pzHe3', 'pxPi', 'pyPi', 'pzPi', 'centrality', 'matter']
 if __name__ == "__main__":
+
+    # v = ROOT.Math.LorentzVector
     # Simulated Hypertritons
     labFrameH = TreeHandler()
+    
+    # # generated
+    # labFrameH.get_handler_from_large_file(
+    #     file_name="../Data/SignalTable_B_20g7.root", 
+    #     tree_name='GenTable',
+    #     preselection='rapidity < 0.5'
+    # )   
+
+    # reconstructed
     labFrameH.get_handler_from_large_file(
         file_name="../Data/SignalTable_B_20g7.root", 
-        tree_name='GenTable',
+        tree_name='SignalTable',
+        preselection='Rapidity < 0.5 and pt > 0.15 and PseudoRapidityHe3 < 0.8 and PseudoRapidityPion < 0.8'
     )   
 
     m_He = 2.80839 #GeV
     m_Pi = 0.139570 #GeV
     
     df=labFrameH.get_data_frame()
+   
+    indices = [i for i in range(0,len(df),1)]
+
+    df['idx']=indices
+    df.set_index('idx', inplace=True)
+
+    # calculate four vectors for remaining events:
+    # momenta in Hypertriton Rest Frame are denoted with q
+    BoostedLorentzVectors = np.array([boost(df=df, i=i) for i in range(len(df))])
+
+    df = pd.concat([df, pd.DataFrame(BoostedLorentzVectors, index=df.index, columns=['mHyp', 'qxHyp', 'qyHyp', 'qzHyp', 'EHe3', 'qxHe3', 'qyHe3', 'qzHe3', 'EPi', 'qxPi', 'qyPi', 'qzPi']
+                                     )], axis=1)
     
-        
+    df['cos_theta_beam'] = [get_cos_theta_beam(df['qxHe3'][i], df['qyHe3'][i], df['qzHe3'][i]) for i in range(len(df))]
+    df['cos_theta_pHyp'] = [get_cos_theta_pHyp(df['qxHe3'][i], df['qyHe3'][i], df['qzHe3'][i], df['pxHe3']+df['pxPi'],df['pyHe3']+df['pyPi'],df['pzHe3']+df['pzPi']) for i in range(len(df))]
 
-
-    # TODO: cut on pt, pseudorapidity, and transversemomentum
-    # TODO: calculate cos theta/theta* for two different axis, once beam and once hypertriton momentum
+    df.write_df_to_root_files(base_file_name='SignalTableDataFrame',
+                              tree_name='df',
+                              path='./',
+                              save_slices=False,
+                              )
     # TODO: plot ?
+    #TODO: write df to root
