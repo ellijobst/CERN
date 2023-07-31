fe3462324dcd9ea2e7fe7a4243b5877d9371ab05
ellijobst <elli.jobst@gmx.de>
2023-07-31 08:56:55 +0200

    Fitting with ROOT

--- Hypertriton_anaylsis/Scripts/apply_model_w_BDT_cut.py
+++ Hypertriton_anaylsis/Scripts/apply_model_w_BDT_cut.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import numpy as np
 import pandas as pd
-import xgboost as xgb
 import matplotlib.pyplot as plt
 import json
 import scipy.optimize as opt
@@ -9,30 +8,48 @@ import scipy.stats as stats
 from hipe4ml.model_handler import ModelHandler
 from hipe4ml.tree_handler import TreeHandler
 from hipe4ml import plot_utils
-from matplotlib.backends.backend_pdf import PdfPages
-from apply_model import apply_ML_model
+from apply_model import apply_ML_model, save_output_as_pdf
 from HypertritonRestframeBoost import get_df
+import ROOT
 
-pt_min = 4
-pt_max = 5
+def signal_and_bkg_fit(m, loc_sig, scale_sig, A_sig, loc_bkg, scale_bkg, A_bkg):
+    return  A_sig*stats.norm.pdf(m, loc=loc_sig, scale=scale_sig)+A_bkg*stats.expon.pdf(m, loc=loc_bkg, scale=scale_bkg)
 
-BDT_cut = -1.7346938775510203
+# select pt cut
+print("Pt_min:")
+pt_min = input()
+print("pt_max:")
+pt_max = input()
 
+# pt_min = 3
+# pt_max = 4
+print(f'{pt_min}<pt<{pt_max}')
+
+# get best BDT cut
+with open(f"../Output/SignEff/significance_{pt_min}_pt_{pt_max}.json", 'r') as f:
+        significance = json.load(f)
+with open(f"../Output/SignEff/efficiency_{pt_min}_pt_{pt_max}.json", 'r') as f:
+        efficiency = json.load(f)
+
+signxeff = [x*y for x,y in zip(significance, efficiency)]
+BDT_cuts = np.linspace(-15,10,100)
+Best_BDT = BDT_cuts[signxeff.index(max(signxeff))]
+print('Best BDT cut:', Best_BDT)
+
+# load data
 data_file = '../Data/DataTable_18_pass3.root'
 data_tree = 'DataTable'
-MC_file = f'../Data/SignalTable_20g7.root'
-bkg_file = f'../Data/DataTable_18LS_pass3.root'
 model_file = f'../Models/Hypertriton_model_{pt_min}<pt<{pt_max}'
-output_file = f"../Output/ML_Hypertriton_output_{pt_min}<pt<{pt_max}_all.pdf"
 
-
-selected_data_hndl, dataH =  apply_ML_model(model_file, data_file, tree_name=data_tree, output_file=None, BDT_cut=BDT_cut, pt_cut=(pt_min, pt_max))
+# apply model to data (cut on ct is included in this function)
+selected_data_hndl, dataH =  apply_ML_model(model_file, data_file, tree_name=data_tree, output_file=None, BDT_cut=Best_BDT, pt_cut=(pt_min, pt_max))
    
-
+# turn into df
 df = selected_data_hndl.get_data_frame()
 print('Number of selected candidates:', len(df))
 print(df.columns)
 
+# plot signal
 plot_utils.plot_distr(
         [selected_data_hndl], 
         column='m', 
@@ -45,8 +62,60 @@ plot_utils.plot_distr(
         alpha=0.5,
         )
 plt.show()
-exit()
-df_cos_theta = get_df(selected_data_hndl)
-print(df_cos_theta[:10])
 
+
+# Convert data to a dictionary with numpy arrays
+data = {key: df[key].values for key in df.keys()}
+print(data)
+
+rdf = ROOT.RDF.FromNumpy(data)
+rdf.Display().Print()
+
+# save as .root file
+rdf.Snapshot('df', 'SelectedData_{pt_min}_pt_{pt_max}.root')
+
+
+
+# plotting with scipy
+'''
+
+plt.figure()
+bins_m = np.linspace(np.amin(df['m']), np.amax(df['m']), 101)
+count_m, bins_m = np.histogram(df['m'], bins=bins_m)
+centered_bins = (bins_m[:-1] + bins_m[1:]) / 2
+
+m_Hyp = 2.991
+p0=[m_Hyp, 3*0.0017, 50, 3, 1, 5]
+# p0=StartParamsNorm(df)
+optimizedParameters, pcov = opt.curve_fit(signal_and_bkg_fit, centered_bins, count_m, p0=p0) #TODO:das funktioniert nicht !
+
+plt.figure()
+plot_utils.plot_distr(selected_data_hndl, column=['m'], bins=100)
+plt.plot(np.linspace(bins_m[0], bins_m[-1], 100), signal_and_bkg_fit(np.linspace(bins_m[0], bins_m[-1], 100), *optimizedParameters), label='Fit')
+save_output_as_pdf(f'../Output/Distribution_m_{pt_min}_pt_{pt_max}.pdf')
+plt.show()
+plt.close()
+
+
+exit()
+'''
+
+# calculate cos theta*
+m_He = 2.80839 #GeV
+m_Pi = 0.139570 #GeV
+
+df_with_cos_theta = get_df(selected_data_hndl)
+print(df_with_cos_theta[:10])
+
+# plot cos theta distribution of Hypertriton Candidates
+bins = np.linspace(-1, 1, 6)
+count_data, bins = np.histogram(df_with_cos_theta['cos_theta_beam'], bins=bins)
+
+plt.stairs(count_data, bins, label=f'Selected Candidates \n {pt_min}<'+r'$p_T$'+f'<{pt_max}', color='orangered')
+plt.ylabel('Counts')
+plt.xlabel(r'$\cos{\theta *}_{beam}$')
+plt.legend()
+save_output_as_pdf(f'../Output/He3Distribution_CosThetaBeam_{pt_min}_pt_{pt_max}.pdf')  
+print('output_saved')
+plt.show()
 
