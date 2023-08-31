fbe6bdb7de9c233cc4997874aa2d2b53a6b1c556
ellijobst <elli.jobst@gmx.de>
2023-08-31 14:41:12 +0200

    Added factor for pt range

--- Hypertriton_analysis/Scripts/ptOptimization3.py
+++ Hypertriton_analysis/Scripts/ptOptimization3.py
@@ -3,17 +3,13 @@ import ROOT
 import logging
 import json
 
-def procedure(rdf_filtered, bkgdf_filtered, gendf_filtered):
-    
-    nreco = rdf_filtered.Count().GetValue()*1/0.99
-    ngen = gendf_filtered.Count().GetValue()
+# TODO: extend BDTEfficiency range to 0.99, binnumber is 90
+def procedure(rdf_filtered, bkgdf_filtered, gendf_filtered, relativeNCand):
 
-    RecoEfficiency = nreco/ngen
-
-    binnumber = 85
+    binnumber = 86
     # Define Arrays
     eff = np.linspace(0.1, 0.95, binnumber)
-    result = [GetSignificance(x, rdf_filtered, bkgdf_filtered, RecoEfficiency) for x in eff]
+    result = [GetSignificance(x, rdf_filtered, bkgdf_filtered, gendf_filtered, relativeNCand) for x in eff]
     sign = [x[0] for x in result]
     nhyp = [x[1] for x in result]
     nbkg = [x[2] for x in result]
@@ -22,33 +18,49 @@ def procedure(rdf_filtered, bkgdf_filtered, gendf_filtered):
     
     return eff, sign, signxeff, nhyp, nbkg
 
-def GetSignificance(x, rdf_filtered, bkgdf_filtered, recoeff):
+def GetSignificance(x, rdf_filtered, bkgdf_filtered, gendf_filtered, relativeNCand):
     print(x)
+
+    nreco = rdf_filtered.Count().GetValue()*1/0.99#check
+    ngen = gendf_filtered.Count().GetValue()
+
+    RecoEfficiency = nreco/ngen
     
     # rdf_new = rdf_filtered.Filter(f"BDTEfficiency < {x}")
     bkgdf_new = bkgdf_filtered.Filter(f"BDTEfficiency < {x}")
     
     Nbkg = bkgdf_new.Count().GetValue()
-    Nhyp = 2*278971416*2.6e-5*0.25*x*recoeff
+    Nhyp = FactorNHyp*x*RecoEfficiency*relativeNCand
     
     return (Nhyp/np.sqrt(Nhyp+Nbkg), Nhyp, Nbkg)
 
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='ptOptimization-28082023.log', level=logging.DEBUG)
+    logging.basicConfig(filename='ptOptimization-31082023.log', level=logging.DEBUG)
     logging.info("Creating Dataframes")
     
     # ROOT.EnableImplicitMT(8)
     
     # Load Dataframes
+
+    # MC reconstructed
     file_path_rdf =  "../Output/DataframesForptOptimization/DataframeNew_*_pt_*_BDTEfficiency.root"
     rdf = ROOT.RDataFrame("df", file_path_rdf)
     
+    # Like Sign Background
     file_path_bkgdf =  "../Output/DataframesForptOptimization/DataframeBkgNew_*_pt_*_BDTEfficiency.root"
     bkgdf = ROOT.RDataFrame("df", file_path_bkgdf)
     
-    gendf = ROOT.RDataFrame("df", "../Output/DataframesForptOptimization/GeneratedHypertritons.root")
+    # MC generated
+    file_path_gendf = "../Output/DataframesForptOptimization/GeneratedHypertritons.root"
+    gendf = ROOT.RDataFrame("df", file_path_gendf)
+
+    # Get Total Number of generated Hypertriton Candidates
+    NGenTotal = gendf.Count().GetValue()
+
+    # Define Factor for Expected Number of Hypertritons Candidates
+    FactorNHyp = 2*278971416*2.6e-5*0.25
  
     # Define Variables for Fit
     m = ROOT.RooRealVar("m", "m [GeV]", 2.97, 3.01)
@@ -59,13 +71,8 @@ if __name__ == "__main__":
     alphar = ROOT.RooRealVar("alphar", "alpha right", 2.08, 1, 2.2)
     nr = ROOT.RooRealVar("nr", "n right", 3.7, 2.9 , 6.5)
 
-
     # Define FitFunction
     fit_func = ROOT.RooCrystalBall("fit_func", "Crystal Ball PDF", m, m0, sigma, alphal, nl, alphar, nr)
-
-
-    # Define Factor for Number of Hypertriton Candidates
-    Nhyp_factor = 2 * 278971416 * 2.6e-5 * 0.25
 
     # Define pt ranges:
     max_int_size = 7#GeV/c
@@ -88,11 +95,16 @@ if __name__ == "__main__":
             pt_min = minimumPt+step_size*j
             pt_max = pt_min + int_size
             logging.warning(f"{pt_min } < pt < {pt_max}")
-            
+
             # Cut on pt
             rdf_filtered = rdf.Filter(f"{pt_min} < pt  and pt <= {pt_max}")
             bkgdf_filtered = bkgdf.Filter(f"{pt_min} < pt  and pt <= {pt_max}")
             gendf_filtered = gendf.Filter(f"{pt_min} < pt  and pt <= {pt_max}")
+
+            # Get Number of generated Hypertriton Candidates in this pt range
+            NGenpt = gendf_filtered.Count().GetValue()
+            relativeNCand = NGenpt/NGenTotal
+            del NGenpt
             
             # Create Histogram for Fit
             inv_mass = rdf_filtered.Histo1D(("InvariantMassHistogram", "hist; m[GeV]", 80, 2.97, 3.01),"m")
@@ -109,19 +121,18 @@ if __name__ == "__main__":
             rdf_filtered = rdf_filtered.Filter(f" 1 < ct and ct < 35")
             
             # Calculate Efficiency, Significance, etc.
-            eff, sign, signxeff, nhyp, nbkg =  procedure(rdf_filtered, bkgdf_filtered, gendf_filtered)
+            eff, sign, signxeff, nhyp, nbkg =  procedure(rdf_filtered, bkgdf_filtered, gendf_filtered, relativeNCand)
             
             # Append the Significance to the Dictionary
             BestBDTSign = sign[signxeff.tolist().index(max(signxeff.tolist()))]
-            logging.info("BestBDTSign: {BestBDTSign}")
+            logging.info(f"BestBDTSign: {BestBDTSign}")
             Significances[f"{pt_min}<pt<{pt_max}"] = BestBDTSign
 
-            inv_mass = None
-            rdf_filered = None
-            bkgdf_filtered = None
-            gendf_filtered = None
+            del inv_mass 
+            del rdf_filtered
+            del bkgdf_filtered 
+            del gendf_filtered 
             
-            
-    with open('ptSignificancesNew.json', 'w') as f:
-        # write the dictionary to the file in JSON format
+    # write the dictionary to the file in JSON format
+    with open('ptSignificances-31082023.json', 'w') as f:
         json.dump(Significances, f)
