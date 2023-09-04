4badd118935f052c60d6fb87e85d6f1dab25938a
ellijobst <elli.jobst@gmx.de>
2023-09-04 12:01:29 +0200

    Scripts from Notebooks

---
+++ Hypertriton_analysis/Scripts/GetBDTEfficiency.py
@@ -0,0 +1,75 @@
+import ROOT
+
+cppcode = """
+#include <ROOT/RDataFrame.hxx>
+#include <TH1.h>
+
+float GetAll(const float &BDT_score, TH1D &hist_cum) {
+    int bins = 5000;
+
+    // Find and return the value at the specified BDT score bin
+    int bin = hist_cum.FindBin(BDT_score);
+    return hist_cum.GetBinContent(bin);
+}
+struct GetBDTEfficiency {
+    TH1D *fHist1D;
+    GetBDTEfficiency(TH1D *h) : fHist1D(h) {}
+
+    float operator()(const float &model_output) {
+       return GetAll(model_output, *fHist1D);
+    }
+};
+"""
+
+ROOT.gInterpreter.ProcessLine(cppcode)
+
+for pt2 in range(4,18,1):
+    
+    # Loop through pt in steps of 0.5GeV
+    pt=pt2/2
+    print(pt)
+    
+    # Define input file names 
+    # NOTE: these Files had the model trained and then applied to it for 0.5GeV pt ranges
+#     filenamebkg = f"DataframesForBDTEfficiency/DataframeBkg_{pt}_pt_{pt+0.5}.root"
+    filename = f"Dataframe_{pt}_pt_{pt+0.5}.root"
+    filenamedata = f"DataframeMC_{pt}_pt_{pt+0.5}.root"
+    
+    # Create Dataframes
+    rdf = ROOT.RDataFrame("df", filename)
+#     rdfbkg = ROOT.RDataFrame("df", filenamebkg)
+    rdfdata = ROOT.RDataFrame("df", filenamedata)
+    
+    # Set Number of bins, and get Number of generated Hypertriton Candidates for this pt range
+#     Ngenpt = ngen[pt2-4]
+    bins = 5000
+    
+    # define Histogram that converts BDTScore into BDTEfficiency with MCs
+    hist = rdf.Histo1D(("BDTScoreHist", "BDT Score", bins, -15, 15),"model_output")
+    del rdf
+    hist_cum = hist.GetCumulative()
+    maximum = hist_cum.GetMaximum()
+    hist_cum.Scale(1/(maximum))
+    hist_cum.Scale(-1)
+    
+    for bin in range(1, bins+1):
+        hist_cum.AddBinContent(bin)
+        
+#     hist_cum.Scale(maximum/Ngenpt)
+#     h = hist_cum
+
+    # Define BDTEfficiencies for the Dataframes
+#     rdf = rdf.Define("BDTEfficiency", ROOT.GetBDTEfficiency(h), ["model_output"])
+    rdfdata = rdfdata.Define("BDTEfficiency", ROOT.GetBDTEfficiency(hist_cum), ["model_output"])
+#     rdfbkg = rdfbkg.Define("BDTEfficiency", ROOT.GetBDTEfficiency(h), ["model_output"])
+    
+    # Cut on BDTEfficiency and save modified DataFrames to .root file
+    treeName = "df"
+#     fileName = f"DataframesForBDTEfficiency/DataframeNew_{pt}_pt_{pt+0.5}_BDTEfficiency.root"
+#     fileNamebkg = f"DataframesForBDTEfficiency/DataframeBkgNew_{pt}_pt_{pt+0.5}_BDTEfficiency.root"
+    fileNamedata = f"DataframeMC_{pt}_pt_{pt+0.5}_BDTEfficiency.root"
+    
+#     rdf = rdf.Filter(f"BDTEfficiency < 0.99").Snapshot(treeName, fileName, {"pt", "Matter",  "centrality", "ct", "m", "model_output", "BDTEfficiency"})
+    rdfdata = rdfdata.Filter(f"BDTEfficiency < 0.99").Snapshot(treeName, fileNamedata)
+#     rdfbkg = rdfbkg.Filter(f"BDTEfficiency < 0.99").Snapshot(treeName, fileNamebkg, {"pt", "Matter",  "centrality", "ct", "m", "model_output", "BDTEfficiency"})
+    
---
+++ Hypertriton_analysis/Scripts/HypertritonRestframeBoost_ROOT.py
@@ -0,0 +1,102 @@
+import ROOT
+
+
+cppcode = """
+#include "Math/Vector4D.h"
+#include "Math/LorentzVector.h"
+#include "Math/Boost.h"
+#include "Math/Vector3D.h"
+
+// Perform Lorentz boost to center-of-mass frame
+ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>> BoostToCMHyp(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& vHyp, const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& v){
+    // Get the beta vector for the boost to the center-of-mass frame
+    ROOT::Math::DisplacementVector3D boostVector = vHyp.BoostToCM();
+    
+    // Create a ROOT::Math::Boost object using the beta vector
+    ROOT::Math::Boost boost(boostVector);
+    
+    // Perform the Lorentz boost using the created Boost object
+    return boost(v);
+    
+};
+
+float GetCosThetaBeam(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& v){
+    float px = v.X();
+    float py = v.Y();
+    float pz = v.Z();
+    return pz/sqrt(px*px+py*py+pz*pz);
+};
+
+float GetCosTheta(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& v, const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& w){
+    ROOT::Math::XYZVector V(v.X(), v.Y(), v.Z());
+    ROOT::Math::XYZVector W(w.X(), w.Y(), w.Z());
+    double s = V.Dot(W);
+    return s/sqrt(V.Mag2()*W.Mag2());
+};
+"""
+ROOT.gInterpreter.ProcessLine(cppcode)
+
+
+# get dataframes and apply preselection
+df_gen = ROOT.RDataFrame("GenTable", "./Data/SignalTable_B_20g7.root").Filter("rapidity < 0.5")
+df_reco = ROOT.RDataFrame("SignalTable", "./Data/SignalTable_B_20g7.root").Filter("Rapidity < 0.5 and PseudoRapidityHe3 < 0.8 and PseudoRapidityPion < 0.8 ")
+
+def HypertritonRestFrameBoost(df_gen, fileName):
+
+    # specify masses of decay daughters
+    mHe3 = 2.80839 #GeV
+    mPi = 0.139570 #Ge
+
+    # define lorentzvectors
+    df_gen = df_gen.Define("LorentzvectorLABHe3", f"ROOT::Math::PxPyPzMVector(pxHe3, pyHe3, pzHe3, {mHe3})")
+    df_gen = df_gen.Define("LorentzvectorLABPi", f"ROOT::Math::PxPyPzMVector(pxPi, pyPi, pzPi, {mPi})")
+
+    # get Hypertriton data
+    df_gen = df_gen.Define("pxHyp", "pxPi+pxHe3")
+    df_gen = df_gen.Define("pyHyp", "pyPi+pyHe3")
+    df_gen = df_gen.Define("pzHyp", "pzPi+pzHe3")
+
+    # get energies
+    df_gen = df_gen.Define("EHe3", f"sqrt(pxHe3*pxHe3+pyHe3*pyHe3+pzHe3*pzHe3+{mHe3}*{mHe3})")
+    df_gen = df_gen.Define("EPi", f"sqrt(pxPi*pxPi+pyPi*pyPi+pzPi*pzPi+{mPi*mPi})")
+    df_gen = df_gen.Define("EHyp", "EPi + EHe3")
+
+    # get mass hyp
+    df_gen = df_gen.Define("mHyp", "sqrt(EHyp*EHyp - pxHyp*pxHyp - pyHyp*pyHyp - pzHyp*pzHyp)")
+
+    # get transverse momentum
+    df_gen = df_gen.Define("pTHe3", "LorentzvectorLABHe3.Pt()")
+    df_gen = df_gen.Define("pTPi", "LorentzvectorLABPi.Pt()")
+
+
+
+
+    # boost in CMS of hyp
+    df_gen = df_gen.Define("LorentzvectorLABHyp", f"ROOT::Math::PxPyPzMVector(pxHyp, pyHyp, pzHyp, mHyp)")
+
+    df_gen = df_gen.Define("LorentzvectorCMSHyp", "BoostToCMHyp(LorentzvectorLABHyp, LorentzvectorLABHyp)")
+    df_gen = df_gen.Define("LorentzvectorCMSHe3", "BoostToCMHyp(LorentzvectorLABHyp, LorentzvectorLABHe3)")
+    df_gen = df_gen.Define("LorentzvectorCMSPi", "BoostToCMHyp(LorentzvectorLABHyp, LorentzvectorLABPi)")
+
+
+    # calculate cos theta* (only for helium)
+    df_gen = df_gen.Define("CosThetaWrtBeam", "GetCosThetaBeam(LorentzvectorCMSHe3)")
+
+
+    df_gen = df_gen.Define("CosThetaWrtpHyp", "GetCosTheta(LorentzvectorCMSHe3, LorentzvectorLABHyp)")
+
+    treeName = "df"
+    fileName = fileName
+    df_gen.Snapshot(treeName, fileName, {"CosThetaWrtBeam", "BDTEfficiency", "CosThetaWrtpHyp", "centrality", "ct", "Matter", "pt", "Rapidity", "m", "PseudoRapidityHe3"})
+
+if __name__ == "__main__":
+    file_path_rdf =  "DataframeData_*_pt_*_BDTEfficiency.root"
+    rdf = ROOT.RDataFrame("df", file_path_rdf)
+
+    HypertritonRestFrameBoost(rdf, "DataframeDatawCosTheta")
+
+    # HypertritonRestFrameBoost(rdf, "DataframeDatawCosTheta")
+
+
+
+
