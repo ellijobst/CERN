1d0e967fd42f9c6f558becdfadf5ff26dd3131c3
ellijobst <elli.jobst@gmx.de>
2023-09-20 13:15:44 +0200

    Minor Changes

--- Hypertriton_analysis/Scripts/HypertritonRestframeBoost_ROOT.py
+++ Hypertriton_analysis/Scripts/HypertritonRestframeBoost_ROOT.py
@@ -1,46 +1,5 @@
 import ROOT
 
-
-cppcode = """
-#include "Math/Vector4D.h"
-#include "Math/LorentzVector.h"
-#include "Math/Boost.h"
-#include "Math/Vector3D.h"
-
-// Perform Lorentz boost to center-of-mass frame
-ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>> BoostToCMHyp(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& vHyp, const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& v){
-    // Get the beta vector for the boost to the center-of-mass frame
-    ROOT::Math::DisplacementVector3D boostVector = vHyp.BoostToCM();
-
-    // Create a ROOT::Math::Boost object using the beta vector
-    ROOT::Math::Boost boost(boostVector);
-
-    // Perform the Lorentz boost using the created Boost object
-    return boost(v);
-};
-
-// Get Cos(theta*) w.r.t. Beam Axis
-float GetCosThetaBeam(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& v){
-    float px = v.X();
-    float py = v.Y();
-    float pz = v.Z();
-    return pz/sqrt(px*px+py*py+pz*pz);
-}
-
-// Get Cos(theta*) w.r.t. an arbitrary Axis
-float GetCosTheta(const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& v, const ROOT::Math::LorentzVector<ROOT::Math::PxPyPzM4D<double>>& w){
-    ROOT::Math::XYZVector V(v.X(), v.Y(), v.Z());
-    ROOT::Math::XYZVector W(w.X(), w.Y(), w.Z());
-    double s = V.Dot(W);
-    return s/sqrt(V.Mag2()*W.Mag2());
-}
-"""
-
-
-# get dataframes and apply preselection
-# df_gen = ROOT.RDataFrame("GenTable", "./Data/SignalTable_B_20g7.root").Filter("rapidity < 0.5")
-# df_reco = ROOT.RDataFrame("SignalTable", "./Data/SignalTable_B_20g7.root").Filter("Rapidity < 0.5 and PseudoRapidityHe3 < 0.8 and PseudoRapidityPion < 0.8 ")
-
 def HypertritonRestFrameBoost(df_gen, fileName):
 
     # specify masses of decay daughters
@@ -69,8 +28,6 @@ def HypertritonRestFrameBoost(df_gen, fileName):
     df_gen = df_gen.Define("pTPi", "LorentzvectorLABPi.Pt()")
 
 
-
-
     # boost in CMS of hyp
     df_gen = df_gen.Define("LorentzvectorLABHyp", f"ROOT::Math::PxPyPzMVector(pxHyp, pyHyp, pzHyp, mHyp)")
 
@@ -79,18 +36,21 @@ def HypertritonRestFrameBoost(df_gen, fileName):
     df_gen = df_gen.Define("LorentzvectorCMSPi", "BoostToCMHyp(LorentzvectorLABHyp, LorentzvectorLABPi)")
 
 
-    # calculate cos theta* (only for helium)
+    # calculate cos theta* wrt beam
     df_gen = df_gen.Define("CosThetaWrtBeam", "GetCosThetaBeam(LorentzvectorCMSHe3)")
 
-
+    # calculate cos theta* wrt to pHyp
     df_gen = df_gen.Define("CosThetaWrtpHyp", "GetCosTheta(LorentzvectorCMSHe3, LorentzvectorLABHyp)")
+
+    # calculate average event plane angle
+    df_gen = df_gen.Define("EPangle", "(EPangleV0A + EPangleV0C)/2")
 
     treeName = "df"
     fileName = fileName
-    df_gen.Snapshot(treeName, fileName, {"CosThetaWrtBeam", "BDTEfficiency", "CosThetaWrtpHyp", "centrality", "ct", "Matter", "pt", "Rapidity", "m", "PseudoRapidityHe3"})
+    df_gen.Snapshot(treeName, fileName, {"Phi", "EPangle", "CosThetaWrtBeam", "BDTEfficiency", "CosThetaWrtpHyp", "centrality", "ct", "Matter", "pt", "Rapidity", "m", "PseudoRapidityHe3"})
 
 if __name__ == "__main__":
-    ROOT.gInterpreter.ProcessLine('#include "MyFunctions.h"')
+    ROOT.gInterpreter.ProcessLine('#include "FunctionsForHypertritonRestFrameBoost.h"')
 
     file_path_rdf =  "DataframeData_*_pt_*_BDTEfficiency.root"
     rdf = ROOT.RDataFrame("df", file_path_rdf)
